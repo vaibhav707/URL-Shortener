53c39aaaae6f1e922f6df62b5a8fea1e876b14af
Vaibhav Bisht <vaibhavbisht707@gmail.com>
2024-12-12 16:13:45 +0530

    Stateless Authentication using JWT

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,4 @@
 const User = require("../models/user")
-const {v4: uuidv4} = require("uuid")
 const {setUser} = require("../service/auth")
 
 async function handleUserSignup(req, res) {
@@ -23,9 +22,8 @@ async function handleUserLogin(req, res) {
             error : "Invalid Username or Password",
         });
     }
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId);
+    const token = setUser(user);
+    res.cookie("uid", token);
     return res.redirect("/");
 }
 
