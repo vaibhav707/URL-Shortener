cdddbfa5fb1f2ddf403c942666b6dfe783cbe535
Vaibhav Bisht <vaibhavbisht707@gmail.com>
2024-12-12 16:13:45 +0530

    Stateless Authentication

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,25 @@
-const sessionIdUser = new Map();
+const jwt = require("jsonwebtoken");
 
-function setUser(id, user) {
-    sessionIdUser.set(id, user);
+const secret = "Demongod$123$";
+
+function setUser(user) {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id) {
-    return sessionIdUser.get(id);
+function getUser(token) {
+    if (!token || token.split('.').length !== 3) {
+        return null;
+    }
+    try {
+        return jwt.verify(token, secret);
+    } 
+    catch (err) {
+        console.error("JWT Verification Error:", err.message);
+        return null;
+    }
 }
 
 module.exports = {
